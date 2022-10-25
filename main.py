81f9ff97fa3046d8cb6589b66019b58c6cd85f96
Parth Sakpal <p.sakpal@share.epsb.ca>
2022-10-25 10:19:29 -0600

    Started Project

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ date-created: 2022-10-20
 """
 
 
-ELEMENT_NAME =
+ELEMENT = "H"
+
+ARRAY = [["H", 1.01, +1], ["He", 4.00, 0], ["Li", 6.94, +1], ["Be", 9.01, +2], []]
+
+for i in range(len(ARRAY)):
+    if ELEMENT == ARRAY[i][0]:
+        print(f"{ARRAY[i][1]}, {ARRAY[i][2]}")
+    else:
+        pass
